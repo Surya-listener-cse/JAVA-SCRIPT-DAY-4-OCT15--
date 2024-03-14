5d617425f4073214e75e80533724eec3fd7b6322
Surya-listener-cse <suryadsp20@gmail.com>
2024-03-14 10:40:48 +0530

    first commit

---
+++ QUESTION2 .js
@@ -0,0 +1,31 @@
+//Question No.2 Resume in JSON Format
+let resume={
+  "Basics Information":{
+      "Name":"SURYA A",
+  "EmailID":"suradsp20@gmail.com",
+  "MobileNo":"6382168907",
+  "Address":"2005 Kamala street, Kadaladi, Kalasapakkam(TK), Tiruvannamalai(DT)"},
+  
+  "Graduation Details" :{ 
+
+  "DIPLOMA":{
+      "Institute":"Sri Sakthi  Polytechnic College",
+      "course":"Diplomo in Mechanical  Engineering",
+      "Year of Passing":"2020",
+      "Percentage":"93.9%"},
+
+  "SSLC":{
+      "Institute":"St.Antony's Matriculation School",
+      "Year of Passing":"2017",
+      "Percentage":"93.5%"}},
+
+  "Skills":{
+          "Languages":"javascript",
+          "Software" : "Adobe Photoshop",
+          "Speaking language":"Tamil and English"}
+           
+  }
+  
+  {
+  console.log(resume);
+  }
